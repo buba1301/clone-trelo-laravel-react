edb987b50336d33dc28521cc8357b791e77f546b
Boris Muzalevskii <bmuza57@gmail.com>
2020-09-02 17:56:35 +0300

    add setHeader func

--- resources/js/utils/index.js
+++ resources/js/utils/index.js
@@ -1,9 +1,6 @@
-import _ from 'lodash';
+export const setDocumentTitle = (title) => document.title = `${title} | Clone Trello`;
 
 export const getErrors = (errors, nameError) => {
-  if (!(_.has(errors, nameError))) {
-    return null;
-  }
-
-  return errors.nameError.join();
+  console.log(errors[nameError].join(''));
+  return errors[nameError].join(''); // подумать как разделить если массив состоит из нескольких ошибок
 };
