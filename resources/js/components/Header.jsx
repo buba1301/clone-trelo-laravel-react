0ae64492f73b89810ce87e3be133e43a7c28bd19
Boris Muzalevskii <bmuza57@gmail.com>
2020-09-03 15:46:51 +0300

    create header with gravatar and signOut button

--- resources/js/components/Header.jsx
+++ resources/js/components/Header.jsx
@@ -1,7 +1,48 @@
 import React from 'react';
+import { Nav, Button } from 'react-bootstrap';
+import ReactGravatar from 'react-gravatar';
+import { asyncActions } from '../slices/index';
 
-const Header = () => (
-    <div>Header</div>
-);
+const Header = ({ user, dispatch, history }) => { // eslint ругается убрать ошибки, все рабоатет
+  const handleSignOutClick = async () => {
+    try {
+      dispatch(asyncActions.signOut());
+      history.push('/sign_in');
+    } catch (e) {
+      console.log(e.response);
+    }
+  };
+
+  const renderUser = (user) => {
+    const { first_name, last_name, email } = user;
+
+    const fullName = [first_name, last_name].join(' ');
+
+    return (
+        <a className="current-user">
+            <ReactGravatar email={email} /> {fullName}
+        </a>
+    );
+  };
+
+  return (
+      <header className="main-header">
+          <Nav as="ul">
+              <Nav.Item as="li">
+                  <Nav.Link href="/">Boards</Nav.Link>
+              </Nav.Item>
+              <Nav.Item as="li">
+                  <Nav.Link href="/">
+                      <span className="logo" />
+                  </Nav.Link>
+              </Nav.Item>
+              <Nav.Item as="li">{renderUser(user)}</Nav.Item>
+              <Button variant="outline-danger" onClick={handleSignOutClick}>
+                  SignOut
+              </Button>
+          </Nav>
+      </header>
+  );
+};
 
 export default Header;
