2d2c22c7ebb373cf04a60df02da649dbcec5ef40
Boris Muzalevskii <bmuza57@gmail.com>
2020-10-03 14:22:33 +0300

    change csrf token mate teg

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,13 +5,12 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name=”csrf-token” content=”{{ csrf_token() }}”>
+    <meta name="csrf-token" content=”{{ csrf_token() }}”>
     <title>Laravel React application</title>
     <link href="{{mix('css/app.css')}}" rel="stylesheet" type="text/css">
 </head>
 
 <body>
-    <h2 style="text-align: center"> Laravel and React application </h2>
     <div id="example"></div>
     <script src="{{mix('js/app.js')}}"></script>
 </body>
