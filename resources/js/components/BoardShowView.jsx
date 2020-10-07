7adc0361c0661c903b98232e3dca9a8f5e91f913
Boris Muzalevskii <bmuza57@gmail.com>
2020-10-07 16:35:31 +0300

    create page logic

--- resources/js/components/BoardShowView.jsx
+++ resources/js/components/BoardShowView.jsx
@@ -1,25 +1,83 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import _ from 'lodash';
 import { useParams } from 'react-router-dom';
-import { Container } from 'react-bootstrap';
+import {
+  Container, Spinner, Navbar, Nav, Button,
+} from 'react-bootstrap';
+import ReactGravatar from 'react-gravatar';
 import { actions, asyncActions } from '../slices/index';
 
+import ModalAddNewUserOnBoard from './ModalAddNewUserOnBoard.jsx';
+import echo from '../bootstrap';
+
 const BoardShowView = () => {
+  const board = useSelector(({ currentBoard }) => currentBoard.board);
+  const members = useSelector(({ currentBoard }) => currentBoard.members);
+  const showModal = useSelector(({ currentBoard }) => currentBoard.showAddNewUserModal);
+
   const { id } = useParams();
+
   const authToken = localStorage.getItem('laravelToken');
-  // const board = useSelector((state) => state.currentBoard.board);
 
   const dispatch = useDispatch();
 
+  const channel = echo.join(`board.${id}`);
+
   useEffect(() => {
-    dispatch(asyncActions.connectToChannel(id, authToken));
-  }, []);
+    dispatch(asyncActions.connectToChannel(channel, authToken));
+  }, [board]);
+
+  if (!board) {
+    return (
+          <Spinner animation="border" role="status">
+              <span className="sr-only">Loading...</span>
+          </Spinner>
+    );
+  }
+
+  const handleModal = async () => {
+    await dispatch(actions.showAddNewUserModal(!showModal));
+  };
+
+  const handleSubmit = async ({ email }, { resetForm }) => {
+    const data = { email, board_id: id };
+    try {
+      await dispatch(asyncActions.addUserOnBoard(data, authToken, channel));
+    } catch (e) {
+      console.log(e.response.data);
+    }
+    dispatch(actions.showAddNewUserModal(!showModal));
+    resetForm();
+  };
+
+  const renderMembers = () => (
+          <>
+            {members.map(({ email, id }) => (
+                    <ReactGravatar email={email} key={id}/>
+            ))}
+          </>
+  );
 
   return (
-      <div>
+      <Container>
+          <Navbar>
+              <Nav className="mr-auto">
+                  <h3>{board.name}</h3>
+              </Nav>
+              {renderMembers()}
+              <Button variant="outline-dark" onClick={handleModal}>
+                  +
+              </Button>
+          </Navbar>
           <div>Test boards {id}</div>
-      </div>
+          <ModalAddNewUserOnBoard
+              showModal={showModal}
+              handleModal={handleModal}
+              handleSubmit={handleSubmit}
+              channel={channel}
+              members={members}
+          />
+      </Container>
   );
 };
 
