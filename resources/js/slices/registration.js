dfb6c3d914012febf45df226a619c5d7f9f78dcc
Boris Muzalevskii <bmuza57@gmail.com>
2020-10-09 17:18:44 +0300

    add fetching and submitbutton state and reducer

--- resources/js/slices/registration.js
+++ resources/js/slices/registration.js
@@ -3,13 +3,21 @@ import axios from 'axios';
 import routes from '../routes';
 import { sessionActions } from './session';
 
-const slice = createSlice({ // add fetching state
+const slice = createSlice({
   name: 'registration',
   initialState: {
     errors: {},
+    regUIState: { disableSubmitButton: false },
+    fetching: false,
   },
   reducers: {
-    registrationErrors: (state, { payload }) => ({ errors: { ...payload } }),
+    registrationErrors: (state, { payload }) => ({ ...state, errors: { ...payload } }),
+    setSubmitButton: (state, { payload }) => {
+      const { regUIState } = state;
+      regUIState.disableSubmitButton = payload;
+      return state;
+    },
+    setFetching: (state, { payload }) => ({ ...state, fetching: payload }),
   },
 });
 
@@ -17,6 +25,8 @@ const registrationActions = slice.actions;
 const registration = slice.reducer;
 
 const singUp = (registrationFormData) => async (dispatch) => {
+  dispatch(registrationActions.setFetching(true));
+
   const url = routes.registrationPath();
 
   const res = await axios.post(url, registrationFormData);
