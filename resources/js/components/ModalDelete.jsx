b289e48739a63025222bd17042b9848eb6d13389
Boris Muzalevskii <bmuza57@gmail.com>
2020-10-12 18:55:22 +0300

    make deleteModal for all delete type

--- resources/js/components/ModalDelete.jsx
+++ resources/js/components/ModalDelete.jsx
@@ -3,19 +3,21 @@ import PropTypes from 'prop-types';
 
 import { Button, Modal } from 'react-bootstrap';
 
-const ModalDelete = ({ showDeleteModal, handleClose, handleDeleteBoard }) => (
+const ModalDelete = ({
+  showDeleteModal, handleClose, handleDelete, type,
+}) => (
     <Modal show={showDeleteModal} onHide={handleClose}>
         <Modal.Header closeButton>
             <Modal.Title>Delete board</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-            Are you sure you want to delete the board?
+            Are you sure you want to delete this {type}?
         </Modal.Body>
         <Modal.Footer>
             <Button variant="secondary" onClick={handleClose}>
                 Close
             </Button>
-            <Button variant="danger" onClick={handleDeleteBoard}>
+            <Button variant="danger" onClick={handleDelete}>
                 Delete
             </Button>
         </Modal.Footer>
