e6dd69c952d4c8e4e2fa77db2e53120329ea8059
Boris Muzalevskii <bmuza57@gmail.com>
2020-10-12 18:53:38 +0300

    make card bootstrap for list view

--- resources/js/components/ListView/ListView.jsx
+++ resources/js/components/ListView/ListView.jsx
@@ -5,42 +5,71 @@ import {
   Button, Form, FormControl, FormGroup, Row, Col, Container, Dropdown, ButtonGroup, DropdownButton,
 } from 'react-bootstrap';
 
-import Header from './Heared.jsx';
+import Header from './Header.jsx';
 import Body from './Body.jsx';
 import Footer from './Footer.jsx';
 
 import { actions, asyncActions } from '../../slices/index';
 
-const ListView = ({ name, id, dispatch }) => {
-  const showAddNewTaskFrom = useSelector(({ currentBoard }) => (currentBoard.tasksUIState.byId[id].showAddNewTaskFrom));
+const ListView = ({
+  name,
+  listId,
+  authToken,
+  dispatch,
+  boardId,
+  fetching,
+  showDeleteModal,
+}) => {
+  const showAddNewTaskFrom = useSelector(({ currentBoard }) => currentBoard.listsUIState.byId[listId].showAddNewTaskFrom);
 
-  const showEditNameListForm = useSelector(({ currentBoard }) => (currentBoard.listsUIState.byId[id].showEditNameListForm));
+  const showEditNameListForm = useSelector(({ currentBoard }) => currentBoard.listsUIState.byId[listId].showEditNameListForm);
+
+  // eslint-disable-next-line camelcase
+  const tasks = useSelector(({ currentBoard }) => currentBoard.tasks.filter(({ list_id }) => list_id === listId));
+  console.log(tasks);
 
   return (
-    <div className="col-lg-3 col-sm-12 col-md-6">
-        <div className="card">
-            <div className="card-header">
-                <Header
-                    name={name}
-                    dispatch={dispatch}
-                    id={id}
-                    showEditNameListForm={showEditNameListForm}
-                />
-            </div>
+      <div className="col-lg-3 col-sm-12 col-md-6">
+          <div className="card">
+              <div className="card-header">
+                  <Header
+                      name={name}
+                      dispatch={dispatch}
+                      listId={listId}
+                      boardId={boardId}
+                      authToken={authToken}
+                      showEditNameListForm={showEditNameListForm}
+                      showDeleteModal={showDeleteModal}
+                      fetching={fetching}
+                  />
+              </div>
 
-            <div className="card-body">
-                <Body />
-            </div>
-            <div className="card-footer">
-                <Footer
-                    dispatch={dispatch}
-                    id={id}
-                    showAddNewTaskFrom={showAddNewTaskFrom}
-                />
-            </div>
-        </div>
-    </div>
+              <div className="card-body">
+                  <ul className="list-group list-group-flush">
+                      {tasks.length !== 0
+                        ? tasks.map((task) => (
+                                <Body
+                                    key={task.id}
+                                    task={task}
+                                    listId={listId}
+                                    dispatch={dispatch}
+                                    authToken={authToken}
+                                    showDeleteModal={showDeleteModal}
+                                />
+                        ))
+                        : null}
+                  </ul>
+              </div>
+              <div className="card-footer">
+                  <Footer
+                      dispatch={dispatch}
+                      listId={listId}
+                      authToken={authToken}
+                      showAddNewTaskFrom={showAddNewTaskFrom}
+                  />
+              </div>
+          </div>
+      </div>
   );
 };
-
 export default ListView;
