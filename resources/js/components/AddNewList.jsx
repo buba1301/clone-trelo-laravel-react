842945ea1fb363f72019ea27c8265e5a2175d42e
Boris Muzalevskii <bmuza57@gmail.com>
2020-10-12 18:36:46 +0300

    clear console.log

--- resources/js/components/AddNewList.jsx
+++ resources/js/components/AddNewList.jsx
@@ -2,16 +2,29 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useFormik } from 'formik';
 import {
-  Button, Form, FormControl, FormGroup, Row, Col, Container,
+  Button, Form, FormControl, FormGroup, Card,
 } from 'react-bootstrap';
 
 import { actions, asyncActions } from '../slices/index';
+import routes from '../routes';
 
-const AddNewList = ({ dispatch, showAddNewListForm }) => {
+const AddNewList = ({
+  dispatch, showAddNewListForm, boardId, authToken,
+}) => {
   const handleOpenForm = () => dispatch(actions.showAddNewListForm(!showAddNewListForm));
 
-  const handleSubmit = (values, { resetForm, setStatus }) => {
-    // здесь добавляем название списка и загружаем в базу данных, после чего отрисовывается список с названием и формой добавления задачи
+  const url = routes.boardsListsPath(boardId);
+  console.log(url);
+
+  const handleSubmit = (values, { resetForm }) => {
+    try {
+      dispatch(asyncActions.createBoardList(values, authToken, boardId));
+      dispatch(actions.showAddNewListForm(!showAddNewListForm));
+      dispatch(actions.setFetching(false));
+      resetForm();
+    } catch (e) {
+      console.log(e.response);
+    }
   };
 
   const f = useFormik({
@@ -24,38 +37,46 @@ const AddNewList = ({ dispatch, showAddNewListForm }) => {
   if (!showAddNewListForm) {
     return (
         <div className="col-lg-3 col-sm-12 col-md-6">
-            <Button onClick={handleOpenForm} block>
-                + Add new list
-            </Button>
+            <div className="card">
+                <div className="card-header">
+                    <Button variant="dark" onClick={handleOpenForm} block>
+                        + Add new list
+                    </Button>
+                </div>
+            </div>
         </div>
     );
   }
 
   return (
-        <div className="col-lg-3 col-sm-12 col-md-6">
-                    <Form>
-                        <FormGroup>
-                            <FormControl
-                              name="name"
-                              type="text"
-                              placeholder="Add new list"
-                              onChange={f.handleChange}
-                              onBlur={f.handleBlur}
-                              value={f.values.name}
-                              // disabled={f.isSubmitting}
-                              // isInvalid={!!f.errors.email}
-                              required
-                            />
-                            <FormControl.Feedback>
-
-                            </FormControl.Feedback>
-                        </FormGroup>
-                        <Button variant="primary" type="submit" block>Add new list</Button>
-                        <Button variant="link" block onClick={handleOpenForm}>
-                            Cancel
-                        </Button>
-                    </Form>
-                </div>
+      <div className="col-lg-3 col-sm-12 col-md-6">
+          <div className="card">
+            <div className="card-header">
+              <Form onSubmit={f.handleSubmit}>
+                  <FormGroup>
+                      <FormControl
+                          name="name"
+                          type="text"
+                          placeholder="Add new list"
+                          onChange={f.handleChange}
+                          onBlur={f.handleBlur}
+                          value={f.values.name}
+                          // disabled={f.isSubmitting}
+                          // isInvalid={!!f.errors.email}
+                          required
+                      />
+                      <FormControl.Feedback></FormControl.Feedback>
+                  </FormGroup>
+                  <Button variant="dark" type="submit" block>
+                      Add new list
+                  </Button>
+                  <Button variant="link" block onClick={handleOpenForm}>
+                      Cancel
+                  </Button>
+              </Form>
+            </div>
+          </div>
+      </div>
   );
 };
 
