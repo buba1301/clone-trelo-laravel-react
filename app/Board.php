ba8eedecbff4a1901ab3a74de53179735ce68ec4
Boris Muzalevskii <bmuza57@gmail.com>
2020-10-12 18:29:01 +0300

    add get lists and tasks method

--- app/Board.php
+++ app/Board.php
@@ -24,4 +24,14 @@ class Board extends Model
     {
         return $this->belongsToMany('App\User', 'user_boards');
     }
+
+    public function lists()
+    {
+        return $this->hasMany('App\BoardList', 'board_id');
+    }
+
+    public function tasks()
+    {
+        return $this->hasManyThrough('App\ListTask', 'App\BoardList', 'board_id', 'list_id');
+    }
 }
